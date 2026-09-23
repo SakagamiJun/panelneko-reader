c346d68496e67c41fb342c6f37c6dbc93cf7c828
SakagamiJun <973175+SakagamiJun@users.noreply.github.com>
2026-09-23 09:11:11 +0800

    feat: define APP_LINKS constants for GitHub repo and issue templates

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_LINKS = {
+  REPO: "https://github.com/SakagamiJun/panelneko-reader",
+  BUG_REPORT: "https://github.com/SakagamiJun/panelneko-reader/issues/new?template=bug_report.md",
+  FEATURE_REQUEST: "https://github.com/SakagamiJun/panelneko-reader/issues/new?template=feature_request.md",
+  ISSUES_CHOOSE: "https://github.com/SakagamiJun/panelneko-reader/issues/new/choose",
+  DISCUSSIONS: "https://github.com/SakagamiJun/panelneko-reader/discussions",
+} as const;
